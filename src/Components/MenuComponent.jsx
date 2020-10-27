3b36698366d6f6e71e1b7085f1ce4b75e4250198
yossef-khaled <yossef.k.y333@gmail.com>
2020-10-27 22:52:02 +0200

    Add onclick listener for each card.

--- src/Components/MenuComponent.jsx
+++ src/Components/MenuComponent.jsx
@@ -7,15 +7,44 @@ class Menu extends Component {
         super(props);
 
         this.state = {
+            slectedDish: null
         };
     }
-    
+
+    onDishSelect = (dish) => {
+        this.setState({slectedDish: dish});
+    } 
+
+    renderDish = (dish) => {
+        console.log("Render function");
+        if(dish != null) {
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <Card key={dish.id}
+                        onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardTitle>{dish.description}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        }
+
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
     state = {  }
     render() { 
         const menu = this.props.dishes.map((dish) => {
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card>
+                    <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -28,6 +57,9 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.slectedDish)}
                 </div>
             </div>
         );
