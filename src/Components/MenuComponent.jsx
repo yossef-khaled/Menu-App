6fe63ccbc059bb25f1830623c5e954caf79e8b5e
yossef-khaled <yossef.k.y333@gmail.com>
2020-10-27 21:29:26 +0200

    Use card instead of media.

--- src/Components/MenuComponent.jsx
+++ src/Components/MenuComponent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Media} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
 class Menu extends Component {
     
@@ -14,25 +14,20 @@ class Menu extends Component {
     render() { 
         const menu = this.props.dishes.map((dish) => {
             return(
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name}/>
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             )
         })
         return ( 
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
                 </div>
             </div>
         );
