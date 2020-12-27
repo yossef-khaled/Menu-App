74568da78f1875ab6e4f6998be21f284d9fb3696
yossef-khaled <yossef.k.y333@gmail.com>
2020-12-27 21:46:55 +0200

    Refactor menu component.

--- src/Components/MenuComponent.jsx
+++ src/Components/MenuComponent.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 import MenuItem from './MenuItemComponent';
 
-function RenderClickedDish({ dish, handleCardClick }) {
-  if (dish != null) {
-    console.log("Render Clicked Dish !!!!");
-    return (
-      <div className="col-12 col-md-5 m-1">
-        <MenuItem dish={dish} handleCardClick={() => handleCardClick(   )} />
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
-}
-
 function Menu(props) {
   
   const menu = props.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
-        <MenuItem dish={dish} handleCardClick={() => props.handleCardClick(dish.id)}/>
+        <MenuItem dish={dish}/>
       </div>
     );
   });
@@ -27,12 +14,6 @@ function Menu(props) {
     <div className="container">
       <div className="row">
         {menu}
-      </div>
-      <div className="row">
-        <RenderClickedDish
-          dish={props.dishes[props.selectedDish]}
-          handleCardClick={() => props.handleCardClick(props.dishes[props.selectedDish])}
-        />
       </div>
     </div>
   );
