2f5c41a42fd20b8b8b5614a4cf0d0db5428ac949
yossef-khaled <yossef.k.y333@gmail.com>
2020-12-27 22:29:45 +0200

    Add navigation breadcrumb to home.

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import RenderCard from './RenderCard';
-
+import {Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 function Home (props) {
     return(
         <div className="container">
+            <div className="row m-2">
+                <Breadcrumb>
+                    <BreadcrumbItem active>Home</BreadcrumbItem>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                </Breadcrumb>
+            </div>
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.dish} />
