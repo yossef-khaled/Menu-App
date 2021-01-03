517bc6d8f2f836c9d0b9088d401f9f23f121b1c5
yossef-khaled <yossef.k.y333@gmail.com>
2021-01-03 11:18:01 +0200

    Use 'react redux form' insted of local state.

--- src/Components/ContactUsComponent.jsx
+++ src/Components/ContactUsComponent.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import {Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Col, Input, Button} from 'reactstrap';
+import {Breadcrumb, BreadcrumbItem, Label, Col, Row, Button} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';   
 
-
-function RenderContactSelect({doesAgree, contactType, handleInputChange}) {
+function RenderContactSelect({doesAgree, contactType }) {
     if(doesAgree) {
         return(
             <Col md={{size: 3, offset: 1}}>
-                <Input type="select" name="contactType"
-                       value={contactType}
-                       onChange={handleInputChange}>
+                <Control.select
+                       className="form-control"
+                       model=".doesAgree" 
+                       name="contactType"
+                       value={contactType}>
                     <option value="none">Choose One</option>
                     <option>Tel.</option>
                     <option>Email</option>
-                </Input>
+                </Control.select>
             </Col>
         );
     }
@@ -22,43 +24,18 @@ function RenderContactSelect({doesAgree, contactType, handleInputChange}) {
             <></>
         );
     }
-}
+};
 
 class ContactUs extends Component {
     
     constructor(props) {
         super(props);
 
-        this.state = {
-            firstName: '',
-            lastName: '',
-            telNum: '',
-            email: '',
-            doesAgree: false,
-            contactType: '',
-            message: ''
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleInputChange(event) {
-        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-        const name = event.target.name;
-        /*Have to use a callback function as 'setState' function takes a while, which will  
-          make the if statement excute before 'setState' even if it's written after it.*/
-        this.setState({ [name]: value}, function () {
-            if(!this.state.doesAgree && this.state.contactType) {
-                console.log(this.state.doesAgree);
-                this.setState({contactType: ""});
-            }
-        });
-    }
-
-    handleSubmit(event) {
-        alert('Current State is: ' + JSON.stringify(this.state));
-        event.preventDefault();
+    handleSubmit(values) {
+        alert('Current State is: ' + JSON.stringify(values));
     }
 
     render() {
@@ -105,8 +82,8 @@ class ContactUs extends Component {
                         <h2 className="mx-auto font-weight-bold mb-5">Reveal It To Us !</h2>
                     </div>
                     <div className="col-12 col-md-9">
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup row>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 {/*The following col element is equivalent to :
                                    <div className="col-md-10">
@@ -114,88 +91,89 @@ class ContactUs extends Component {
                                    </div>
                                 */}
                                 <Col md={10}>
-                                <Input type="text" 
+                                <Control.text  
+                                       className="form-control"
+                                       model=".firstName"
                                        id="firstName"
                                        name="firstName"
                                        placeholder="Type Your First Name"
-                                       value={this.state.firstName}
-                                       onChange={this.handleInputChange}
                                        />
                                 </Col>
-                            </FormGroup>
-                            <FormGroup row>
+                            </Row>
+                            <Row className="form-group">
                                 <Label htmlFor="lastName" md={2}>Last Name</Label>
                                 <Col md={10}>
-                                <Input type="text"
+                                <Control.text 
+                                       className="form-control"
+                                       model=".lastName"
                                        id="lastName"
                                        name="lastName" 
                                        placeholder="Type Your Last Name"
-                                       value={this.state.lastName}
-                                       onChange={this.handleInputChange}
                                        />
                                 </Col>
-                            </FormGroup>
-                            <FormGroup row>
+                            </Row>
+                            <Row className="form-group">
                                 <Label htmlFor="telNum" md={2}>Tel. Number</Label>
                                 <Col md={10}>
-                                <Input type="text" 
+                                <Control.text 
+                                       className="form-control"
+                                       model=".telNum" 
                                        id="telNum" 
                                        name="telNum" 
                                        placeholder="Type Your Telephone Number"
-                                       value={this.state.telNum}
-                                       onChange={this.handleInputChange}
                                        />
                                 </Col>
-                            </FormGroup>
-                            <FormGroup row>
+                            </Row>
+                            <Row className="form-group">
                                 <Label htmlFor="email" md={2}>Email</Label>
                                 <Col md={10}>
-                                <Input type="email" 
+                                <Control.text  
+                                       className="form-control"
+                                       model=".email"
                                        id="email" 
                                        name="email" 
                                        placeholder="Type Your Email"
-                                       value={this.state.email}
-                                       onChange={this.handleInputChange}
                                        />
                                 </Col>
-                            </FormGroup>
-                            <FormGroup row>
+                            </Row>
+                            <Row className="form-group">
                                 <Label htmlFor="message" md={2}>Message</Label>
                                 <Col md={10}>
-                                <Input type="textarea" 
+                                <Control.textarea 
+                                       className="form-control"
+                                       model=".message"
                                        id="message" 
                                        name="message" 
                                        placeholder="Type Whatever you want"
-                                       value={this.state.message}
-                                       onChange={this.handleInputChange}
                                        />
                                 </Col>
-                            </FormGroup>
-                            <FormGroup row className="text-centered">
+                            </Row>
+                            <Row className="text-centered form-group">
                                 <Col className="text-center mt-2">
-                                    <Input type="checkbox" 
+                                    <Control.check  
+                                           className="form-control"
+                                           model=".doesAgree"
                                            id="doesAgree" 
-                                           name="doesAgree" 
-                                           value={this.state.doesAgree}
-                                           onChange={this.handleInputChange}
+                                           name="doesAgree"
                                            />
                                     <Label className="font-weight-bold"> May We Contact You ?</Label>
                                 </Col>
-                                <RenderContactSelect doesAgree={this.state.doesAgree} contactType={this.state.contactType} handleInputChange={this.handleInputChange}/>
-                            </FormGroup>
-                            <FormGroup className="text-center">
+
+
+                            </Row>
+                            <Row className="text-center form-group">
                                 <Col>
                                     <Button type="submit" color="primary">
                                         Send Feedback
                                     </Button>
                                 </Col>
-                            </FormGroup>
-                        </Form>
+                            </Row>
+                        </LocalForm>
                     </div>
                 </div>
             </div>
         );
     }
-}
+};
 
 export default ContactUs;
