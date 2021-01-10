e02615eef2ae9a4aaab5d6708d4b0c168a2090b7
yossef-khaled <yossef.k.y333@gmail.com>
2021-01-10 10:58:36 +0200

    Recombine reducers in the main store.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { combineReducers, createStore } from 'redux';
+import { Dishes } from './dishes';  
+import { Leaders } from './leaders';  
+import { Promotions } from './promotions';  
+import {Comments} from './comments';  
 
 export const ConfigureStore = () => {
           const store = createStore(
-                    Reducer,
-                    initialState
+                    combineReducers({
+                              dishes: Dishes,
+                              leaders: Leaders,
+                              promotions: Promotions,
+                              comments: Comments
+                    })
           );
 
           return store;
