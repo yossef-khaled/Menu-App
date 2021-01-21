d5753e5bdb982a8cdfe9454a772bbc5e386d90e5
yossef-khaled <yossef.k.y333@gmail.com>
2021-01-21 20:29:15 +0200

    Convert dishdetail into a class component.

--- src/Components/DishDetailComponent.jsx
+++ src/Components/DishDetailComponent.jsx
@@ -1,61 +1,73 @@
+import React, { Component } from 'react';
 import {
     Row, Card, CardImg, CardTitle, CardText, Breadcrumb, BreadcrumbItem, Button, 
     Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from "reactstrap";  
 import {Link} from 'react-router-dom';
 import '../App.css'
 
-function DishDetail({dish, comments}) {
-    const dishComments = comments.map((comment) => {
-        return(
-            <div className="m-3">
-                <hr/>
-                <CardTitle tag="h4">
-                    {comment.author}
-                </CardTitle>
-                <CardText>
-                    {comment.comment}
-                </CardText>           
+class  DishDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+
+        const dishComments = this.props.comments.map((comment) => {
+            return(
+                <div className="m-3">
+                    <hr/>
+                    <CardTitle tag="h4">
+                        {comment.author}
+                    </CardTitle>
+                    <CardText>
+                        {comment.comment}
+                    </CardText>           
+                </div>
+            );
+        });
+
+        return( 
+            <div className="container col-10 m-4"> 
+                <Row>
+                    <div className="row m-2">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                       <div className="col-12">
+                            <h3>{this.props.dish.name}</h3>
+                            <hr />
+                        </div> 
+                    </div>
+                </Row>
+                <Row className="align-items-start">
+                    <CardImg width="100%" className="col-6" src={this.props.dish.image} alt={this.props.dish.name} />
+                    <Card className="col-5">
+                        <CardTitle className="m-2" tag="h4">
+                                {this.props.dish.name} :
+                        </CardTitle>
+                        <CardText className="col-15">
+                            - {this.props.dish.description}
+                        </CardText>
+                        <div className="container">
+                            <h3>Comments :</h3>
+                            {dishComments.length > 0 ? dishComments : <p className="ml-3 text-muted">- There is no comments for this dish</p>}
+                        </div>
+                    <Button outline color="info" className="submitComment" width="50%">Submit Comment</Button>
+                    </Card>
+                </Row>
+                <Modal>
+                    <ModalHeader isOpen={}>
+                        Submit Comment
+                    </ModalHeader>
+                </Modal>
             </div>
         );
-    });
-
-    return (
-        <div className="container col-10 m-4"> 
-            <Row>
-                <div className="row m-2">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                   <div className="col-12">
-                        <h3>{dish.name}</h3>
-                        <hr />
-                    </div> 
-                </div>
-            </Row>
-            <Row className="align-items-start">
-                <CardImg width="100%" className="col-6" src={dish.image} alt={dish.name} />
-                <Card className="col-5">
-                    <CardTitle className="m-2" tag="h4">
-                            {dish.name} :
-                    </CardTitle>
-                    <CardText className="col-15">
-                        - {dish.description}
-                    </CardText>
-                    <div className="container">
-                        <h3>Comments :</h3>
-                        {dishComments.length > 0 ? dishComments : <p className="ml-3 text-muted">- There is no comments for this dish</p>}
-                    </div>
-                <Button outline color="info" className="submitComment" width="50%">Submit Comment</Button>
-                </Card>
-            </Row>
-            <Modal>
-                {/*TODO...
-                   a modal that contains a form to submit a comment 
-                 */}
-            </Modal>
-        </div>
-    );
+    }
 }
 
 export default DishDetail;
