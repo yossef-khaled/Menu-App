54c0ac7a891a57468640e03eea3fc126d2715daa
yossef-khaled <yossef.k.y333@gmail.com>
2021-04-15 01:39:37 +0200

    Dispatcher to make use of actions.

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -7,6 +7,7 @@ import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import DishDetail from './DishDetailComponent';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -17,6 +18,10 @@ const mapStateToProps = state => {
     selectedDish: state.selectedDish 
   };
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+});
 
 class Main extends Component {
 
@@ -47,6 +52,7 @@ class Main extends Component {
       return(
         <DishDetail dish={this.props.dishes[parseInt(match.params.dishId)]}
                     comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
         />
       );
     }
@@ -67,5 +73,5 @@ class Main extends Component {
   }
 }
  
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
