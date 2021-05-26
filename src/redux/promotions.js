112cb3e2b66119d92e31fae68d90f4d27df61b63
yossef-khaled <yossef.k.y.333@gmail.com>
2021-05-26 19:29:02 +0200

    Handle promotions actions.

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,8 +1,21 @@
-import { PROMOTIONS } from '../Shared/promotions';
+import * as ActionTypes from './ActionTypes';
 
-export const Promotions = (state = PROMOTIONS, action) => {
+export const Promotions = (state = {
+    isLoading: false,
+    errMess: null,
+    promos: []
+}, action) => {
           switch(action.type) {
-                    default:
-                              return state;
+                case ActionTypes.ADD_PROPMOS:
+                    return {...state, isLoading: false, errMess: null, promos: action.payload};
+                    
+                case ActionTypes.PROMOS_LOADINFG:
+                    return {...state, isLoading: true, errMess: null, promos: []}
+    
+                case ActionTypes.PROPMOS_FAILED:
+                    return {...state, isLoading: false, errMess: action.payload, promos: []};
+                
+                default:
+                    return state;
           }
 };
