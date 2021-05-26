e089b0cf1496992312e3eaf3bf8e3b8453815123
yossef-khaled <yossef.k.y.333@gmail.com>
2021-05-26 19:26:19 +0200

    Add action types for each type of data.

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,4 +1,9 @@
 export const ADD_COMMENT = "ADD_COMMENT";
+export const ADD_DISHES = "ADD_DISHES"; 
 export const DISHES_LOADING = "DISHES_LOADING";
 export const DISHES_FAILED = "DISHES_FAILED";
-export const ADD_DISHES = "ADD_DISHES"; 
+export const ADD_COMMENTS = "ADD_COMMENTS";
+export const COMMENTS_FAILED = "COMMENTS_FAILED";
+export const ADD_PROPMOS = "ADD_PROPMOS";
+export const PROMOS_LOADINFG = "PROMOS_LOADINFG";
+export const PROPMOS_FAILED = "PROPMOS_FAILED";
