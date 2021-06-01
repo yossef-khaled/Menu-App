37f58486c093c3a8e209bc8a45d1b88869d95a15
yossef-khaled <yossef.k.y333@gmail.com>
2021-06-01 18:34:16 +0200

    insert the comment into the comments array.

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -10,7 +10,7 @@ export const Comments = (state = {
                     comment.id = state.length;
                     comment.date = new Date().toISOString();
                     console.log(`The recently added comment is : ${comment.rating} ${comment.author} ${comment.comment}`);
-                    return state.concat(comment);
+                    return state.comments.concat(comment);
 
                 case ActionTypes.ADD_COMMENTS:
                     return {...state, errmess: null, comments: action.payLoad};
